9fb8dc783c5028c3a23ef998432661157784e58c
renanrcs <renan.cavalcante@yahoo.com>
2023-07-05 15:34:00 -0300

    payroll-api

---
+++ payroll-api/src/test/java/io/github/renanrcs/payrollapi/PayrollApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.renanrcs.payrollapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
