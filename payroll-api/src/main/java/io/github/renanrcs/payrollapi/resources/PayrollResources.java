ca954d266f426d2941ac30f37c0472e076ff4903
renanrcs <renan.cavalcante@yahoo.com>
2023-07-06 13:42:45 -0300

    Implementing OpenFeign in communication between APIs

--- payroll-api/src/main/java/io/github/renanrcs/payrollapi/resources/PayrollResources.java
+++ payroll-api/src/main/java/io/github/renanrcs/payrollapi/resources/PayrollResources.java
@@ -1,22 +1,31 @@
 package io.github.renanrcs.payrollapi.resources;
 
 import io.github.renanrcs.payrollapi.domain.Payroll;
+import io.github.renanrcs.payrollapi.domain.User;
+import io.github.renanrcs.payrollapi.feignClientes.UserFeign;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "api/payments")
 public class PayrollResources {
 
+    private final UserFeign userFeign;
+
     @GetMapping(value = "/{workerId}")
     public ResponseEntity<Payroll> getPayment(@PathVariable Long workerId, @RequestBody Payroll payment){
+
+        User user = userFeign.findById(workerId).getBody();
+
         return ResponseEntity.ok().body(
                 new Payroll(
-                        "Renan",
+                        user.getName(),
                         payment.getDescription(),
-                        payment.getHourlyPrice(),
-                        100.0,
-                        payment.getHourlyPrice() * payment.getWorkedHours()
+                        user.getHourlyPrice(),
+                        payment.getWorkedHours(),
+                        user.getHourlyPrice() * payment.getWorkedHours()
                         )
         );
     }
