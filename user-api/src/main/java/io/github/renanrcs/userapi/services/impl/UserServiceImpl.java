15889cd06133d4eb784f930986d70cc89e2511f7
renanrcs <renan.cavalcante@yahoo.com>
2023-07-11 13:39:03 -0300

    eureka server created

--- user-api/src/main/java/io/github/renanrcs/userapi/services/impl/UserServiceImpl.java
+++ user-api/src/main/java/io/github/renanrcs/userapi/services/impl/UserServiceImpl.java
@@ -5,17 +5,22 @@ import io.github.renanrcs.userapi.repositories.UserRepository;
 import io.github.renanrcs.userapi.services.UserService;
 import io.github.renanrcs.userapi.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final Environment env;
 
     @Override
     public User findById(Long id) {
+        log.info("USER_SERVICE ::: Get request on " + env.getProperty("local.server.port") + " port");
         return userRepository.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("Object not found" + "test commit"));
     }
